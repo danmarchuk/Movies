d2ae91da7d5857a5ccf134b6ba05f77d1062aaad
Danyil Marchuk <zemero58@gmail.com>
2023-08-30 14:58:44 +0200

    add fonts

--- Movies/Constants.swift
+++ Movies/Constants.swift
@@ -11,5 +11,6 @@ import UIKit
 
 struct K {
     static let launchScreenBlue = UIColor(red: 0.01, green: 0.15, blue: 0.25, alpha: 1.0)
+    static let loginButtonBlue = UIColor(red: 0.35, green: 0.78, blue: 0.98, alpha: 1.0)
     
 }
